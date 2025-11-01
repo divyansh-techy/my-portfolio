bd572afe3ec7b04b670895d0a197b8efedc6f62c
Divyansh Upadhyay <divyanshupadhyay05@gmail.com>
2025-11-02 00:42:45 +0530

    Created the Navbar section

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+// src/components/Navbar.jsx
+
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav>
+      <div className="nav-logo">
+        <a href="#">Divyansh techy</a> 
+      </div>
+      <ul className="nav-links">
+        <li><a href="#about">About</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#skills">Skills</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
